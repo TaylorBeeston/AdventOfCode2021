8ba0e3d3317f5c1d53c315535c8b26494c15b6ca
Taylor Beeston <beeston.taylor@gmail.com>
2021-12-03 22:47:02 -0800

    :beers: Make progress on day 4 puzzle 1

--- day4puzzle1/src/bingo.rs
+++ day4puzzle1/src/bingo.rs
@@ -1,1 +1,98 @@
-pub struct Board {}
+struct BoardNumber {
+    pub value: usize,
+    pub checked: bool,
+}
+
+impl BoardNumber {
+    pub fn new(value: usize, checked: bool) -> Self {
+        BoardNumber { value, checked }
+    }
+}
+
+pub struct Board {
+    values: Vec<Vec<BoardNumber>>,
+}
+
+impl Board {
+    pub fn new(values: &str) -> Self {
+        let board_values: Vec<Vec<BoardNumber>> = values
+            .lines()
+            .map(|line| {
+                line.split(" ")
+                    .map(|value| BoardNumber::new(value.parse::<usize>().unwrap(), false))
+                    .collect()
+            })
+            .collect();
+
+        Board {
+            values: board_values,
+        }
+    }
+
+    pub fn check_num(&self, number: usize) -> Self {
+        let new_values: Vec<Vec<BoardNumber>> = self
+            .values
+            .iter()
+            .map(|row| {
+                row.iter()
+                    .map(|column| {
+                        if column.value == number {
+                            BoardNumber::new(column.value, true)
+                        } else {
+                            BoardNumber::new(column.value, column.checked)
+                        }
+                    })
+                    .collect()
+            })
+            .collect();
+
+        Board { values: new_values }
+    }
+
+    pub fn check_won(&self) -> bool {
+        if self
+            .values
+            .iter()
+            .any(|row| row.iter().all(|column| column.checked))
+        {
+            return true;
+        }
+        if self.values[0]
+            .iter()
+            .enumerate()
+            .any(|(index, _column)| self.values.iter().all(|row| row[index].checked))
+        {
+            return true;
+        }
+
+        false
+    }
+
+    pub fn unmarked_sum(&self) -> usize {
+        self.values
+            .iter()
+            .map(|row| {
+                row.iter()
+                    .map(|column| if column.checked { column.value } else { 0 })
+                    .sum::<usize>()
+            })
+            .sum()
+    }
+
+    pub fn print(&self) {
+        println!("---------");
+
+        self.values.iter().for_each(|row| {
+            let line: String = row
+                .iter()
+                .map(|column| {
+                    let checked = if column.checked { "✅" } else { "" };
+                    format!("{} {}", column.value, checked)
+                })
+                .collect();
+            println!("{}", line);
+        });
+
+        println!("---------");
+    }
+}
--- day4puzzle1/src/main.rs
+++ day4puzzle1/src/main.rs
@@ -1,6 +1,33 @@
 mod bingo;
 mod input;
 
+use crate::bingo::Board;
+use crate::input::{get_boards, get_input};
+
 fn main() {
-    println!("Hello, world!");
+    let mut boards: Vec<Board> = get_boards().iter().map(|input| Board::new(input)).collect();
+    let raw_input = get_input();
+    let mut input = raw_input.iter();
+
+    let mut number = input.next();
+
+    while number.is_some() && !boards.iter().any(|board| board.check_won()) {
+        println!("Checking {}", number.unwrap());
+
+        boards = boards
+            .iter()
+            .map(|board| board.check_num(*number.unwrap()))
+            .collect();
+
+        number = input.next();
+    }
+
+    boards.iter().for_each(|board| {
+        if board.check_won() {
+            println!("We have a winner! Total score is {}", board.unmarked_sum());
+            println!("Last number was {}", number.unwrap());
+            println!("Answer is {}", number.unwrap() * board.unmarked_sum());
+        }
+        // board.print();
+    });
 }
